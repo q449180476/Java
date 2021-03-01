7edab65a20d95e325d5b970ad8f471e3f15a15de
lwh <449180476@qq.com>
2021-03-01 20:21:28 +0800

    day02

--- src/main/java/day02/LiteralDemo.java
+++ src/main/java/day02/LiteralDemo.java
@@ -23,12 +23,44 @@ public class LiteralDemo {
         byte m = 120;
         int n = m;//小转大,右边的m是小类型,给左边的大类型n直接赋值.
 
-        double f=3.2;
-        float s=3.2f;
-        f=s;
+        float f=3.2f;
+        double d2=f;//小转大 float->double
 
-        x=(long)b;
-        x=a;
+        /**大到小，强制转（显示转换）*/
+        int h=128;
+        byte b3=(byte)x;
+        System.out.println(b3);
 
+        float f2=33.5f;
+        int i=(int)f2;
+        System.out.println(i);
+
+        /**运算规则*/
+        /**1.计算结果的数据类型，与最大类型一致*/
+        System.out.println(3/2);//1.int/int，得到的结果还是int类型
+        System.out.println(3/2d);//1.5 int/double，得到的结果是double
+        /**2.byte short char三种比int小的整数，
+         * *运算时先转换成int在进行计算*/
+         byte j=-1;
+         byte k=-128;
+         byte l=(byte)(j+k);
+        System.out.println(l);
+        /**3.整数运算溢出*/
+        /**
+         * 溢出的现象：
+         * 因为做了大的整数数据的运算，超出了int的范围
+         * 解决办法：把整数int类型提升成long类型
+         **/
+        //需求：光运行一年的长度是多少米？3亿m/s
+        System.out.println(300000000L*60*60*24*365);
+
+        /**4.浮点数运算不精确*/
+        /**这是一个现象，后续会学习解决方案*/
+        System.out.println((float) (1-0.8));
+
+        /**5.浮点数的特殊值*/
+        System.out.println(3.14/0);//Infinity--无穷大
+        System.out.println(0/0.0);//NaN-Not a Number
+        System.out.println(0.0/0);//NaN-Not a Number
     }
 }
